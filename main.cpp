93882fd0677974163ee9f06a1d67391980954a6b
Michal Kalita <kalita.michal@gmail.com>
2015-12-19 18:42:57 +0100

    Added reading metrix from file

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <fstream>
 #include <regex>
 
-
 using namespace std;
 
 void make_bigger(double *&array, unsigned int &size);
@@ -24,11 +23,171 @@ bool inline file_exist(const string &name);
 
 double get_number();
 
-int main() {
+double **load_matrix_user(unsigned int &rows, unsigned int &cols);
+
+double **load_matrix_file(unsigned int &rows, unsigned int &cols);
+
+double **load_matrix_file(char *file_ch, unsigned int &rows, unsigned int &cols);
+
+int main(int argc, char **args) {
     double **array = NULL;
-    unsigned int rows = 0, cols = 0, array_rows = 1, array_cols = 10;
+    unsigned int rows = 0, cols = 0;
     int iterator;
     char character;
+
+    if (argc == 1) { // zadna data v parametrech
+        cout << "Nacist matici ze souboru? [a/n]: ";
+        cin >> character;
+
+        if (character == 'a' || character == 'A') {
+            array = load_matrix_file(rows, cols);
+        } else {
+            cout << "load_matrix_user" << endl;
+            array = load_matrix_user(rows, cols);
+        }
+    }
+
+    if (argc == 2) {
+        cout << "load_matrix_file" << endl;
+        array = load_matrix_file(args[1], rows, cols);
+    }
+    else if (argc > 2) {
+        cout << "Nerozpoznane parametry programu" << endl;
+        cout << "program [input_file]" << endl;
+        cout << "program - nazev programu" << endl;
+        cout << "input_file - soubor s matici\n"
+                "\ttextovy soubor, na kazdem radku je stejny pocet cisel,"
+                "cisla jsou oddelena bilym znakem" << endl;
+        return 1;
+    }
+
+    print(array, cols, rows);
+
+    double **triangle = gauss_elimination(array, cols, rows);
+    if (triangle != 0) {
+        cout << "Vypocteno" << endl;
+        print(triangle, cols, rows);
+
+        for (iterator = 0; iterator < rows; ++iterator) {
+            cout << "x" << iterator + 1 << " = " << triangle[iterator][cols - 1] << endl;
+        }
+
+        cout << "Chcete ulozit do souboru? [a/n]: ";
+//        cin.ignore(INT_MAX, '\n'); // maze vsechna data na vstupu
+        cin >> character;
+        if (character == 'a' || character == 'A' || character == '\n') {
+            saveToFile(triangle, cols, rows);
+        }
+    } else {
+        cout << "Nelze vypocitat pomoci gaussovy eliminacni metody" << endl;
+    }
+
+    if (triangle != 0) {
+        delete[] triangle[0];
+        for (iterator = 1; iterator < rows; ++iterator) {
+            delete[] triangle[iterator];
+        }
+        delete[] triangle;
+    }
+
+    delete[] array[0];
+    for (iterator = 1; iterator < rows; ++iterator) {
+        delete[] array[iterator];
+    }
+    delete[] array;
+
+    return 0;
+}
+
+double **load_matrix_file(char *file_ch, unsigned int &rows, unsigned int &cols) {
+    ifstream file;
+    file.open(file_ch);
+
+    if (!file.is_open()) {
+        cout << "Chyba, soubor nelze otevrit" << endl;
+        return nullptr;
+    }
+
+    unsigned int array_rows = 3, array_cols = 4;
+    double **array = NULL;
+
+    array = new double *[array_rows]; // pole ukazatelu, bude obsahovat adresy radku
+    array[0] = new double[array_cols]; // prvni radek matice
+
+    cols = rows = 0;
+
+    // nacintani prvniho radku matice
+    while (file.peek() != '\n') {
+        if (cols >= array_cols) { // neni misto v poli, je treba zvetsit pole
+            make_bigger(array[0], array_cols);
+        }
+        file >> array[0][cols];
+        ++cols;
+    }
+
+    if (cols == 0) {
+        cout << "konec, zadna data" << endl;
+        return nullptr;
+    }
+
+    rows++;
+
+    // pokud pole je vetsi nez pocet hodnot, optimalizuje pole
+    if (cols != array_cols) {
+        optimize_array_size(array[0], cols);
+        array_cols = cols;
+    }
+
+    bool skip_create = false, ok;
+    // nacitani dalsich radku
+    for (; !file.eof(); ++rows) { // projde cely soubor a pricita pocet radku
+        if (rows >= array_rows) { // overuje, zda jsou volne pozice
+            make_bigger(array, array_rows);
+        }
+
+        if (skip_create) // vytvori novy radek pokud je potreba
+            skip_create = false;
+        else
+            array[rows] = new double[cols];
+
+        ok = false;
+        for (int col = 0; col < cols; ++col) { // prochazeni sloupcu
+            file >> array[rows][col]; // nacteni hodnoty
+            if (array[rows][col] != 0) {
+                ok = true; // stav radku
+            }
+        }
+
+        if (!ok) { // pokud v radku jsou vsechny hodnoty 0, opakuje nacitani do stejne adresy
+            rows--;
+            skip_create = true;
+        }
+    }
+
+    if (rows != array_rows) {
+        optimize_array_size(array, rows);
+        array_rows = rows;
+    }
+
+    return array;
+}
+
+double **load_matrix_file(unsigned int &rows, unsigned int &cols) {
+    char file[256];
+    while (1) {
+        cin.ignore(INT_MAX, '\n'); // maze vsechna data na vstupu
+        cout << "Nazev souboru: ";
+        cin.getline(file, 256);
+        if (file_exist(file))
+            break;
+        cout << "Chyba, soubor neexistuje" << endl;
+    }
+    return load_matrix_file(file, rows, cols);
+}
+
+double **load_matrix_user(unsigned int &rows, unsigned int &cols) {
+    unsigned int array_rows = 3, array_cols = 4;
+    double **array;
 
     cout << "Nazdar, zadej matici:" << endl;
 
@@ -46,7 +205,7 @@ int main() {
 
     if (cols == 0) {
         cout << "konec, zadna data" << endl;
-        return 1;
+        return nullptr;
     }
 
     rows++;
@@ -80,49 +239,14 @@ int main() {
 
     cout << "konec nacitani, zadana matice " << cols << "x" << rows << endl;
 
-    print(array, cols, rows);
-
-    double **triangle = gauss_elimination(array, cols, rows);
-    if (triangle != 0) {
-        cout << "Vypocteno" << endl;
-        print(triangle, cols, rows);
-
-        for (iterator = 0; iterator < rows; ++iterator) {
-            cout << "x" << iterator + 1 << " = " << triangle[iterator][cols - 1] << endl;
-        }
-
-        cout << "Chcete ulozit do souboru? [A/n]: ";
-        cin.ignore(INT_MAX, '\n'); // maze vsechna data na vstupu
-        cin.get(character);
-        if (character == 'a' || character == 'A' || character == '\n') {
-            saveToFile(triangle, cols, rows);
-        }
-    } else {
-        cout << "Nelze vypocitat pomoci gaussovy eliminacni metody" << endl;
-    }
-
-    if (triangle != 0) {
-        delete[] triangle[0];
-        for (iterator = 1; iterator < array_rows; ++iterator) {
-            delete[] triangle[iterator];
-        }
-        delete[] triangle;
-    }
-
-    delete[] array[0];
-    for (iterator = 1; iterator < array_rows; ++iterator) {
-        delete[] array[iterator];
-    }
-    delete[] array;
-
-    return 0;
+    return array;
 }
 
 double get_number() {
     double number;
     cin.clear();
     cin >> number;
-    while(cin.fail()) {
+    while (cin.fail()) {
         cout << endl;
         cout << "Spatny vstup, zadejte cislo: ";
         cin.clear();
@@ -266,10 +390,10 @@ void saveToFile(double **array, unsigned int cols, unsigned int rows) {
             "<body>\n"
             "\n"
             "<h1>Matice " << rows << "x" << cols << "</h1>\n"
-            "<h2>Zadani</h2>\n"
+            "<h2>Vypoctena matice</h2>\n"
             "<table>\n";
 
-    for(int row = 0; row < rows; row++) {
+    for (int row = 0; row < rows; row++) {
         file << "<tr>\n";
         for (int col = 0; col < cols; ++col) {
             file << "<td>" << array[row][col] << "</td>\n";
@@ -281,7 +405,7 @@ void saveToFile(double **array, unsigned int cols, unsigned int rows) {
             "<h2>Vysledky</h2>\n";
 
     for (int i = 0; i < rows; ++i) {
-        file << "<h3>x<sub>" << i+1 << "</sub> = " << array[i][cols-1] << "</h3>\n";
+        file << "<h3>x<sub>" << i + 1 << "</sub> = " << array[i][cols - 1] << "</h3>\n";
     }
 
     file << "</body>\n"
